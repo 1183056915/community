1aad522b580ee7cbe4f1e9abfe6963a1475e4d34
1183056915 <1183056915@qq.com>
2019-07-28 14:01:55 +0800

    check

--- src/main/java/cn/lc/community/mapper/UserMapper.java
+++ src/main/java/cn/lc/community/mapper/UserMapper.java
@@ -3,11 +3,14 @@ package cn.lc.community.mapper;
 import cn.lc.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) value (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
      void insert(User user);
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
