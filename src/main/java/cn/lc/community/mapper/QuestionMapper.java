230cc4a274ea81e6270216956587e23950a76001
1183056915 <1183056915@qq.com>
2019-08-05 22:14:47 +0800

    pagination

--- src/main/java/cn/lc/community/mapper/QuestionMapper.java
+++ src/main/java/cn/lc/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package cn.lc.community.mapper;
 import cn.lc.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,6 +13,12 @@ public interface QuestionMapper {
     @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tags) values (#{title},#{description},#{gmtCreate}," +
             "#{gmtModified},#{creator},#{tags})")
     void create(Question qusetion);
-    @Select("select * from question")
-    List<Question> list();
+    @Select("select * from question limit #{offset},#{size}")
+    List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
+    @Select("select count(1) from question")
+    Integer count();
+    @Select("select * from where creator = #{userId} question limit #{offset},#{size}")
+    List<Question> listByUserId( @Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(@Param("userId")Integer userId);
 }
--- src/main/java/cn/lc/community/service/QuestionService.java
+++ src/main/java/cn/lc/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package cn.lc.community.service;
 
+import cn.lc.community.dto.PaginationDTO;
 import cn.lc.community.dto.QuestionDTO;
 import cn.lc.community.mapper.QuestionMapper;
 import cn.lc.community.mapper.UserMapper;
@@ -18,9 +19,20 @@ public class QuestionService {
     private QuestionMapper questionMapper;
     @Autowired
     private UserMapper userMapper;
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page < 1){
+            page = 1;
+        }
+        if(page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for(Question question:questions){
           User user = userMapper.findById(question.getCreator());
          QuestionDTO questionDTO = new QuestionDTO();
@@ -28,6 +40,33 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+    public PaginationDTO list(Integer userId, Integer page, Integer size){
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.countByUserId(userId);
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page < 1){
+            page = 1;
+        }
+        if(page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for(Question question:questions){
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
