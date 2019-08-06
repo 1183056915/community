e8370c095cf7466b7dbec210eb3049933a9c212c
1183056915 <1183056915@qq.com>
2019-08-06 13:51:47 +0800

    config

--- src/main/java/cn/lc/community/controller/PublishController.java
+++ src/main/java/cn/lc/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package cn.lc.community.controller;
 
 import cn.lc.community.mapper.QuestionMapper;
-import cn.lc.community.mapper.UserMapper;
 import cn.lc.community.model.Question;
 import cn.lc.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,8 +16,7 @@ public class PublishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
@@ -49,21 +45,7 @@ public class PublishController {
         }
 
 
-        User user = null;
-
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user = (User)request.getSession().getAttribute("user");
         if(user == null){
             model.addAttribute("error","用户未登录");
             return "publish";
